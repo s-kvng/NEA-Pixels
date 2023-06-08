1d39547201178e0dd254315318d527af49044146
nathan <kvngnathan8420@gmail.com>
2023-06-08 05:40:00 +0000

    Add search functionality

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
+// useLocation hook
+import { useLocation } from 'react-router-dom';
+
+// useFetch hook
+import useFetch from '../hooks/useFetch';
+
+// components
+import CategoryNav from '../components/CategoryNav';
+import Product from '../components/Product';
+
 const Search = () => {
+
+  const location = useLocation()
+  const searchParams = new URLSearchParams(location.search)
+  const searchTerm = searchParams.get('query');
+
+  console.log(searchTerm)
+
+
   return <div>Search</div>;
 };
 
